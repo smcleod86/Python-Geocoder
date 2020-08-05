9a62b666bb182e6aa3ccde2789ab9fdbaf523d1b
smcleod86 <mcleod.scott_86@yahoo.com>
2020-08-05 11:40:24 -0700

    Scott McLeod

--- solutions/solution.py
+++ solutions/solution.py
@@ -19,14 +19,14 @@ def geocode(list):
   API_BASE_URL = "https://api.darksky.net/forecast/c73c375afbedccd447f98b4e275bb84c/37.8267,-122.4233"
 
   for city in list:
-  point = geocoder.arcgis(city)
-  point.json
+  g = geocoder.arcgis(city)
+  g.json
 
   full_api_url = API_BASE_URL + {g.lat} + "," + {g.lng}
   result = requests.request('GET', full_api_url).json()
 
 
-  print(f'the {city} is located at latitude: {point.lat} & longitude: {point.lng}')
+  print(f'the {city} is located at latitude: {g.lat} & longitude: {g.lng}')
   print(f'At the {city} right now, it\'s: {result.summary} with a current temperature of {result.temperature}')
 
 geocode(destinations) 
